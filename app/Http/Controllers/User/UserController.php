b73d308ae59f9020b718b2aa0ff1f079d0be6c17
Dimuthu-Pinsara <dimuthupinsara@gmail.com>
2023-09-24 01:40:27 +0530

    do1 : Create Login

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    function login(Request $request){
+        $user = DB::table('users')->where('email',$request->email)->first();
+        if ( !$user || !Hash::check($request->password,$user->password)){
+            return ["error"=>"error passowrd"];
+        }
+        return $user;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\BillController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', [BillController::class, 'index'])->name('test');
+Route::post('/login', [UserController::class, 'login'])->name('login');
